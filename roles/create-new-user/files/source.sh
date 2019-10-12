164ea14bce161fd9147e5e86f63e5bd39f0c83e9
Nikhilesh <nikhileshxiii@gmail.com>
2019-10-12 10:07:25 -0500

    Add source.sh

---
+++ roles/create-new-user/files/source.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export GOPATH=/home/nik/golib
+export PATH=$PATH:$GOPATH/bin
+export GOPATH=$GOPATH:/home/nik/code/golang/wksp
+
+export PATH="$PATH:$HOME/npm/bin"
+
+export NODE_PATH="$NODE_PATH:$HOME/npm/lib/node_modules"
+source ~/.sdkman/bin/sdkman-init.sh
+export DOCKER_HOST=unix:///var/run/docker.sock
