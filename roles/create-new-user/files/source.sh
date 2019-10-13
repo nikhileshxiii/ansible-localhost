81f6240f02fbb895615081a4f4e4d72bba5be719
Nikhilesh <nikhileshxiii@gmail.com>
2019-10-13 10:09:42 -0500

    Add HELM_HOST

--- roles/create-new-user/files/source.sh
+++ roles/create-new-user/files/source.sh
@@ -8,3 +8,4 @@ export PATH="$PATH:$HOME/npm/bin"
 export NODE_PATH="$NODE_PATH:$HOME/npm/lib/node_modules"
 source ~/.sdkman/bin/sdkman-init.sh
 export DOCKER_HOST=unix:///var/run/docker.sock
+export HELM_HOST=localhost
