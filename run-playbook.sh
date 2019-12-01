1d0e1d5dc12e7a88eda93561410d7b60be4e6f24
Nikhilesh <nikhileshxiii@gmail.com>
2019-12-01 15:12:03 -0600

    Update: Remove installation and removal of venv.

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
-python3 -m venv venv
+
+#rm -rf venv
+
+#python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+#ansible-playbook -i hosts play.yml --extra-vars "ansible_sudo_pass="
 ansible-playbook -i hosts play.yml -K
 
-rm -rf ansible
+#rm -rf venv
