8c11316fd89ddadddc9caf937a70891130e1a6c0
Nikhilesh <nikhileshxiii@gmail.com>
2019-12-01 12:27:56 -0600

    Add bashrc, zsh and source.sh files.

--- roles/create-new-user/files/source.sh
+++ roles/create-new-user/files/source.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
-export GOPATH=/home/nik/golib
+export GOPATH=${HOME}/golib
 export PATH=$PATH:$GOPATH/bin
-export GOPATH=$GOPATH:/home/nik/code/golang/wksp
+# export GOPATH=$GOPATH:${HOME}/code/golang/wksp
 
 export PATH="$PATH:$HOME/npm/bin"
 
 export NODE_PATH="$NODE_PATH:$HOME/npm/lib/node_modules"
-source ~/.sdkman/bin/sdkman-init.sh
 export DOCKER_HOST=unix:///var/run/docker.sock
 export HELM_HOST=localhost
+
+
+if [ -f ~/.sdkman/bin/sdkman-init.sh ]
+    source ~/.sdkman/bin/sdkman-init.sh
+fi
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+[[ -s "/home/{{ username }}/.sdkman/bin/sdkman-init.sh" ]] && source "/home/{{ username }}/.sdkman/bin/sdkman-init.sh"
