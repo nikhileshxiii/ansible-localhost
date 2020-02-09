93f3807d3c0389fcf91b47dc8ac1fbaf1d1076a8
Nikhilesh Vinnakota <nikhileshxiii@gmail.com>
2020-02-09 12:40:59 -0600

    Refactor stuff

--- roles/create-new-user/files/source.sh
+++ roles/create-new-user/files/source.sh
@@ -1,6 +1,6 @@
+export GOROOT=/usr/local/go
 export GOPATH=${HOME}/go
-export PATH=$PATH:$GOPATH/bin
-# export GOPATH=$GOPATH:${HOME}/code/golang/wksp
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 export PATH="$PATH:$HOME/npm/bin"
 
