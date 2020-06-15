6d1afce55489b9f200ecab376fd1ae6624e3a730
Nikhilesh Vinnakota <nikhileshxiii@gmail.com>
2020-06-15 00:34:44 -0500

    Cleanup

--- run-playbook.sh
+++ run-playbook.sh
@@ -4,10 +4,11 @@ rm -rf venv
 
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+# pip install -r requirements.txt
+pip install ansible
 
 #ansible-playbook -i hosts main.yml --extra-vars "ansible_sudo_pass="
 # ansible-playbook -i hosts main.yml -K --skip-tags "kali"
-ansible-playbook -c local main.yml --tags "ubuntu"
+ansible-playbook main.yml --tags "debian" --skip-tags "vpnauth" -K
 
 #rm -rf venv
