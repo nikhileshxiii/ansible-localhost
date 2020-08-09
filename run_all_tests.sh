b0c4edce5673c64216f35a426ec64fe2155aa906
Nikhilesh Vinnakota <nikhileshxiii@gmail.com>
2020-08-09 18:25:32 +0000

    Update shell script, travis and github workflows files

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,13 +2,14 @@
 
 # rm -rf venv
 
-# python3 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 # pip install -r requirements.txt
 pip install ansible
 pip install yamllint
 pip install ansible-lint
 pip install molecule
+pip install docker
 
 echo ""
 echo "==> Run yamllint"
@@ -19,5 +20,7 @@ ansible-playbook --syntax-check main.yml
 
 echo "==> Run ansible-lint"
 ansible-lint main.yml
+
+bash ./run_molecule_tests.sh
 
 # ansible-playbook main.yml --tags "debian" --skip-tags "vpnauth" -K
---
+++ run_molecule_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source venv/bin/activate
+
+for i in $(ls roles/); do
+	echo "Running role $i molecule test"
+	cd roles/${i} && molecule test
+	if [ $? -eq 0 ] 
+	then
+		echo "${i} molecule test status: SUCCESS" >> result.txt
+	else
+		echo "${i} molecule test status: FAILURE" >> result.txt
+	fi
+	cd ../..
+done
