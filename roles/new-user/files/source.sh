80719621095331ca6df12399b701b9cefdf173cf
Nikhilesh Vinnakota <Nikhileshxiii@gmail.com>
2020-08-30 17:08:27 -0500

    Updatesource.sh, remove goroot and other stuff

--- roles/new-user/files/source.sh
+++ roles/new-user/files/source.sh
@@ -1,13 +1,14 @@
-export GOROOT=/usr/local/go
+# export GOROOT=/usr/local/go
 export GOPATH=${HOME}/go
-export SNAP_PATH=/snap
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:$SNAP_PATH/bin
+# export SNAP_PATH=/snap
+export PATH=$PATH:$GOPATH/bin
+# export PATH=$PATH:$GOROOT/bin:$GOPATH/bin:$SNAP_PATH/bin
 
 export PATH="$PATH:$HOME/npm/bin"
 
 export NODE_PATH="$NODE_PATH:$HOME/npm/lib/node_modules"
 export DOCKER_HOST=unix:///var/run/docker.sock
-export HELM_HOST=localhost
+# export HELM_HOST=localhost
 
 
 if [ -f ~/.sdkman/bin/sdkman-init.sh ]
